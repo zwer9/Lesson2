aa92a16c5353751e1ac123ce1f09d6ce8823749a
zwer9 <ktlusty9@gmail.com>
2024-04-07 16:35:13 +0200

    Lekcja o tablicach

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] tab = new double[5];
+        int[] cos = {1, 2, 3, 4, 5};
+        String[] napisy = {"Kupa1", "Kupa2", "Kupa3"};
+        String[] cosie = new String[3];
+        System.out.println(tab[0] + " " + cos[0] + " " + napisy[0]);
+
+    }
+
+}
